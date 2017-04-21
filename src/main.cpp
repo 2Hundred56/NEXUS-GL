3a8b7b0797a7ef9bcd005013277a5c1c42f68719
Meculus (Gold Rebirth) <ethan.saff@gmail.com>
2017-04-21 18:29:21 -0400

    - Added SDL and GLAD - Made main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Apr 21, 2017
+ *      Author: sunlightiv
+ */
+#include "SDL2/SDL.h"
+#include "glad/glad.h"
+int main() {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	return 0;
+}
+
+
+
